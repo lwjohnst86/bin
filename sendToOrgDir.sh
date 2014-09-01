00da42085012b2368c2330f5b48cc412a76f6b75
Luke W Johnston <lwjohnst@gmail.com>
2014-09-01 15:32:46 -0400

    Updated scripts to move org files to dir + update them
    I needed a way to copy TODO org files into the org dir so I can view
    them with agenda + mobileorg. So I created two scripts, one that
    copied it over and another that updates them (via crontab).

--- sendToOrgDir.sh
+++ sendToOrgDir.sh
@@ -4,7 +4,8 @@
 ## the parent directory name, and then sends it to the org file
 ## directory.  I need some way to be able to uniquely name my
 ## research-project TODO.org files so that I have it on my MobileOrg +
-## agenda view.
+## agenda view.  The updateScript -> updateTODO.sh script will be
+## update the sent TODO files via crontab
 
 indir=$(cd $(dirname "$1"); pwd -P)
 infile=$(basename "$1")
@@ -14,5 +15,4 @@ ext=${infile##*.}
 updateScript=~/bin/updateTODO.sh
 
 cp -vu $indir/$infile $outdir/$outfile.$ext
-
 sed -i -e "/origtodo=/a $indir\/$infile" $updateScript
--- updateTODO.sh
+++ updateTODO.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
-indir=$(cd $(dirname "$1"); pwd -P)
-infile=$(basename "$1")
+## This script updates the ~/org TODO files from various research
+## projects that have been added by `sendToOrgDir.sh` script in the
+## ~/bin folder.
+## 
+## The orgtodo variable is edited by the `sendToOrgDir.sh` file.  This
+## script is set in the crontab to run frequently (~15 min) to update
+## the TODO list.
+
+date=$(date +"%F")
 outdir=~/org
-outfile=$(basename "$indir")
-ext=${infile##*.}
-updateScript=~/bin/updateTODO.sh
-outfilepath=~/
 origtodo="
+/home/luke/Documents/graduate/phd/duties/nsgsa/TODO.org
+/home/luke/bin/prodigen/TODO.org
+/home/luke/Documents/research-projects/sfaDiabetes/TODO.org
+/home/luke/Documents/research-projects/renalProg/TODO.org
+
 "
 
-
-echo $origFiles
-
-for file in $origfiles
+for file in $origtodo
 do
-    cp -uv $infilepath $outdir/$(basename 
+    outfile=$outdir/$(basename $(dirname $file)).${file##*.}
+    if [[ `diff -q $file $outfile` ]]
+    then
+        if [ $file -nt $outfile ]
+        then
+            cp -uv $file $outfile
+        elif [ $outfile -nt $file ]
+        then
+            cp -uv $outfile $file
+        fi
+    fi
 done
+
