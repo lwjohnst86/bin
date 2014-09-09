85c54e34b074a5585153c51659a7d77e9d51f274
Luke W Johnston <lwjohnst@gmail.com>
2014-09-09 15:57:16 -0400

    Created script to reduce a PDF size to be smaller

---
+++ reducePDF.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script reduces the size of a pdf to a specified resolution.  It
+# uses GhostScript.
+
+filename=`basename $1`
+dir=$(cd $(dirname "$1"); pwd -P)
+outname=out_$filename
+resolution=/ebook
+
+gs -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.4 \
+    -dUseCIEColor \
+    -dPDFSETTINGS=$resolution \
+    -dNOPAUSE \
+    -dQUIET \
+    -dBATCH \
+    -sOutputFile=$dir/$outname $dir/$filename
+
+#rm $dir/$(basename "$1")
+#mv $dir/$outname $dir/$(basename "$1")
