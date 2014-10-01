0bc2453011ab59ecee3ed17262ec549cab50033a
Luke W Johnston <lwjohnst@gmail.com>
2014-09-30 22:21:04 -0400

    Created script to help with commenting SAS
    I needed a way to quickly make a header for a SAS macro file and be
    able to use easier tags than <b> for bold, etc.  So I created this
    script which adds a header to a macro file or replaces quick tags such
    as `` with the html proper (<code>).

---
+++ sasQuickTags.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [[ "$2" == "--header" ]]
+then
+    echo Adding header
+    echo "/*!
+
+    ...short title...
+
+    * @author
+    * @created
+
+    */
+
+/**
+
+    ... long title of macro goes here...
+    \\\\
+
+    ... description of macro goes here...
+    \\\\
+
+    [Examples:] &
+
+    * @param
+    * @return
+
+    */
+$(cat $1)" > $1
+
+elif [[ "$*" != "--header" ]]
+then
+    echo Replacing quick tags with real code
+    sed -i -e 's/\\\\$/<p>/g' -e 's/``\(.\)/<code>\1/g' \
+        -e 's/\(.\)``/\1<\/code>/g' -e 's/ \[\(.\)/ <b>\1/g' \
+        -e 's/\(.\)\]/\1<\/b>/g' -e 's: &$:<br>:g' \
+        $1
+fi
+
