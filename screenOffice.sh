b795d8f36115e0fbf6232453f650724e250e79f3
Luke W Johnston <lwjohnst@gmail.com>
2014-10-15 21:23:08 -0400

    Moved feh background command to bottom of script

--- screenOffice.sh
+++ screenOffice.sh
@@ -8,9 +8,11 @@ IN="LVDS1"
 
 if (xrandr | grep "$EXT connected"); then
     xrandr --output $EXT --auto --output $IN --off && xrandr -r 75
-    feh --bg-max "$(find ~/Pictures/wallpaper/ -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
     sed -i "$ c\ screen=$EXT" ~/.config/redshift.conf
 else
     xrandr --output $IN --auto
     sed -i "$ c\ screen=0" ~/.config/redshift.conf
 fi
+
+# Set the wallpaper
+feh --bg-max "$(find /home/luke/Pictures/wallpaper/ -iname '*.jpg' -o -iname '*.png' | shuf -n1)"
