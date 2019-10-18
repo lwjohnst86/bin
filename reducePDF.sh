0f44ccbc0c4e371790659d917df06730759f179f
Luke Johnston <lwjohnst@gmail.com>
2019-10-18 10:19:46 +0200

    update reducing pdf script, don't need those extra commands

--- reducePDF.sh
+++ reducePDF.sh
@@ -6,17 +6,12 @@
 filename=`basename $1`
 dir=$(cd $(dirname "$1"); pwd -P)
 outname=out_$filename
-resolution=/ebook
+resolution=/screen
 
 gs -sDEVICE=pdfwrite \
-    -dCompatibilityLevel=1.4 \
-    -dUseCIEColor \
     -dPDFSETTINGS=$resolution \
-    -dNOPAUSE \
-    -dQUIET \
-    -dBATCH \
-    -dCompressFonts=true \
-    -sOutputFile=$dir/$outname $dir/$filename
+    -q \
+    -o $dir/$outname $dir/$filename
 
 #rm $dir/$(basename "$1")
 #mv $dir/$outname $dir/$(basename "$1")
